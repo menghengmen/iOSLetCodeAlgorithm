817e189a80ed98e4a51b0177434ed602933a7c2c
孟恒 <1215933389@qq.com>
2019-10-20 22:36:18 +0800

    3 最长非重复子字符串算法

--- LetCodeAlgorithm/LetCodeAlgorithm/ViewController.m
+++ LetCodeAlgorithm/LetCodeAlgorithm/ViewController.m
@@ -23,7 +23,7 @@ struct ListNode {
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //   算法题描述： 给定一个整数数组 nums 和一个目标值         target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+    //   算法题描述：1 给定一个整数数组 nums 和一个目标值         target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
     //    你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
     //    示例:
     //    给定 nums = [2, 7, 11, 15], target = 9
@@ -32,7 +32,34 @@ struct ListNode {
 
         [self calcalateWithNumbers:@[@1,@2,@3,@5,@6] target:7];
     
+// 3 最长非重复子字符串算法 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+//
+//  示例 1:
+//  输入: "abcabcbb"
+//  输出: 3
+//  解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+    NSString *str = @"gasdkgkasgjdnafgdgda";
+    NSMutableSet*set = [NSMutableSet new];
+      long n = str.length;
+      int ans = 0, i = 0, j = 0 ,index = 0;
+      while (i<n && j<n) {
+          if (![set containsObject:[str substringWithRange:NSMakeRange(j,1)]]) {
+               [set addObject:[str substringWithRange:NSMakeRange(j++, 1)]];
+                    if (j - i > ans) {
+                         ans = j - i;
+                         index = i;
+                       }
+                 }else{
+              
+             [set removeObject:[str substringWithRange:NSMakeRange(i++, 1)]];
+          }
+       
+   }
+      NSLog(@"%@",[str substringWithRange:NSMakeRange(index, ans)]);
+
 }
+
 -(void)calcalateWithNumbers:(NSArray*)arr target:(int)target{
     for (int i = 0; i <arr.count-1; i ++) {
         for (int j = i +1; j <arr.count; j ++) {
