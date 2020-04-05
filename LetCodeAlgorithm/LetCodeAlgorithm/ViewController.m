1f27722be7a5f65998f43f66e7c6d688d02827c8
menghengmen <1215933389@qq.com>
2020-04-05 17:40:02 +0800

    新增算法

--- LetCodeAlgorithm/LetCodeAlgorithm/ViewController.m
+++ LetCodeAlgorithm/LetCodeAlgorithm/ViewController.m
@@ -72,7 +72,26 @@ struct ListNode {
     NSString *str1 = [sepArr lastObject];
     NSLog(@"最后一个单词的长度为%lu",(unsigned long)str1.length);
     
+//    5: 题目描述
+//    写出一个程序，接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写。
+//    输入描述:
+//    第一行输入一个有字母和数字以及空格组成的字符串，第二行输入一个字符。
+//    输出描述:
+//    输出输入字符串中含有该字符的个数。
+    [self getDuplicateSubStrCountInCompleteStr:@"aaaabcdef" withSubStr:@"a"];
+    
 }
+
+//利用替换先把重复元素替换掉,再根据length长度做判断
+- (NSInteger )getDuplicateSubStrCountInCompleteStr:(NSString *)completeStr withSubStr:(NSString *)subStr
+{
+   
+   return  completeStr.length - [completeStr stringByReplacingOccurrencesOfString:subStr withString:@""].length;
+}
+
+
+
+
 
 
 
