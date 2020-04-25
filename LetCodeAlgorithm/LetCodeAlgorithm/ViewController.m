5b330dd3cac36a12adc6fae244a2a7627657daa8
menghengmen <1215933389@qq.com>
2020-04-25 19:04:32 +0800

    新增算法
    开发一个坐标计算工具， A表示向左移动，D表示向右移动，W表示向上移动，S表示向下移动。从（0,0）点开始移动，从输入字符串里面读取一些坐标，并将最终输入结果输出到输出文件里面。

--- LetCodeAlgorithm/LetCodeAlgorithm/ViewController.m
+++ LetCodeAlgorithm/LetCodeAlgorithm/ViewController.m
@@ -120,7 +120,53 @@ struct ListNode {
     // 13:将一个英文语句以单词为单位逆序排放。例如“I am a boy”，逆序排放后为“boy a am I”
     //所有单词之间用一个空格隔开，语句中除了英文字母外，不再包含其他字符
     [self reverseString:@"I am a boy"];
+//    //14:  开发一个坐标计算工具， A表示向左移动，D表示向右移动，W表示向上移动，S表示向下移动。从（0,0）点开始移动，从输入字符串里面读取一些坐标，并将最终输入结果输出到输出文件里面。
+//    输入：
+//    合法坐标为A(或者D或者W或者S) + 数字（两位以内）
+//    坐标之间以;分隔。
+//    非法坐标点需要进行丢弃。如AA10;  A1A;  $%$;  YAD; 等。
+//    下面是一个简单的例子 如：
+//    A10;S20;W10;D30;X;A1A;B10A11;;A10;
+//    处理过程：
+//    起点（0,0）
+//    +   A10   =  （-10,0）
+//    +   S20   =  (-10,-20)
+//    +   W10  =  (-10,-10)
+//    +   D30  =  (20,-10)
+//    +   x    =  无效
+//    +   A1A   =  无效
+//    +   B10A11   =  无效
+//    +  一个空 不影响
+//    +   A10  =  (10,-10)
+//    结果 （10， -10）
+    [self calcalateCoordinates:@[@"a10",@"s20",@"w10",@"d30",@"x",@"a70",@"1b1"]];
     
+}
+
+-(CGPoint)calcalateCoordinates:(NSArray *)coordsArr{
+    int x = 0;
+    int y = 0;
+    for (int i =0 ; i <coordsArr.count;  i++) {
+        if ([[NSString stringWithFormat:@"%@",coordsArr[i]] hasPrefix:@"a"]
+            ||[[NSString stringWithFormat:@"%@",coordsArr[i]] hasPrefix:@"s"]
+            ||[[NSString stringWithFormat:@"%@",coordsArr[i]] hasPrefix:@"a"]
+            ||[[NSString stringWithFormat:@"%@",coordsArr[i]] hasPrefix:@"d"]) {
+            int coord = [[[NSString stringWithFormat:@"%@",coordsArr[i]] substringFromIndex:1] intValue];
+            NSString *preStr = [NSString stringWithFormat:@"%@",coordsArr[i]];
+            if ([preStr hasPrefix:@"a"]) {
+                x -= coord;
+            } else if ([preStr hasPrefix:@"s"]){
+                y -= coord;
+            } else if ([preStr hasPrefix:@"w"]){
+                y += coord;
+            } else if ([preStr hasPrefix:@"d"]){
+                x += coord;
+            }
+        
+        }
+    }
+    CGPoint point = CGPointMake(x, y);
+    return point;
 }
 
 
