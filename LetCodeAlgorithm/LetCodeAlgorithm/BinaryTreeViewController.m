05dbde19dde42fcc64dfd19bfe940e4729ea1f96
menghengmen <1215933389@qq.com>
2020-05-08 23:13:22 +0800

    二叉树相关
    二叉树前驱节点与后继节点

--- LetCodeAlgorithm/LetCodeAlgorithm/BinaryTreeViewController.m
+++ LetCodeAlgorithm/LetCodeAlgorithm/BinaryTreeViewController.m
@@ -30,11 +30,57 @@
     there.leftNode = [[BinaryTreeNode alloc]initWithVal:6];
     there.rightNode = [[BinaryTreeNode alloc]initWithVal:9];
     
+    /*         4
+          2          7
+     
+       1     3     6     9
+     
+     
+     */
+    
     NSMutableArray *arr = [NSMutableArray new];
     self.dataArr = arr;
     [self preorderTravelsal:one];
+    for (int i =0; i <self.dataArr.count; i ++) {
+        BinaryTreeNode *node = self.dataArr[i];
+        NSLog(@"+-+--=-=-%ld",node.val);
+    }
+    [self nextPoint:one];
+
+}
+
+
+/// 前驱节点
+/// @param node 二叉树
+-(void)prePoint:(BinaryTreeNode *)node{
+    /// prepoint = node.left.right.right.right......
+    if (node.leftNode != nil) {
+        BinaryTreeNode *leftNode = node.leftNode;
+        while (leftNode.rightNode !=nil) {
+            leftNode = leftNode.rightNode;
+        }
+        NSLog(@"前驱节点%ld",(long)leftNode.val);
+    }
     
 }
+
+
+
+/// 后继节点
+/// @param node 二叉树
+-(void)nextPoint:(BinaryTreeNode *)node{
+    
+    if (node.rightNode !=nil) {
+        BinaryTreeNode *rightNode = node.rightNode;
+        while (rightNode.leftNode != nil) {
+            rightNode = rightNode.leftNode;
+        }
+       NSLog(@"后继节点%ld",rightNode.val);
+
+    }
+
+}
+
 
 
 /// 前序遍历（递归）
